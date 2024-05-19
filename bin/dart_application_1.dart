de3f279a52a4c6c1998cf6b4db7bea60132c1c23
daniel12142007 <danijelahatzanov@gmail.com>
2024-05-19 19:53:19 +0600

    array and set

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,5 +1,35 @@
-void main(List<String> arguments) {
-  String firstName = 'Daniel';
-  int age = 17;
-  print('Hi I\'m $firstName and I\'m $age');
+void main() {
+  // String firstName = 'Daniel';
+  // int age = 17;
+  // print('Hi I\'m $firstName and I\'m $age');
+
+  // dynamic dyn = '1';
+  // dyn = 2;
+  // dyn = 3.33;
+  // dyn = false;
+
+  // int a = 20, b = 5;
+  // print('+ : ${a + b}');
+  // print('- : ${a - b}');
+  // print('/ : ${(a / b).toInt()}');
+  // print('~/ : ${a ~/ b}');
+  // print('* : ${a * b}');
+  // print('% : ${a % b}');
+
+  // int a = 11;
+  // int result = a >= 6 ? 15 : 25;
+  // print(result);
+
+  // var array = [1, 2, 3, 4, 5, 6];
+  // array.add(7);
+  // array.add(8);
+  // array.add(9);
+  // array.add(10);
+  // array.addAll([11, 12, 13, 14, 15]);
+  // array.remove(4); //удаляет переменую по значению
+  // array.removeAt(0); //удаляет переменую по index
+  // array.removeWhere(
+  //     (element) => element < 10); //удаляет элементы по заданому условию
+  // print(array);
+  // var set = {1,1,2,3,4,5,6};//Set<int> сохраняет только уникальные значение
 }
