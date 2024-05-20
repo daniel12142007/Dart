a431af7d75677f4cd98f7a9066c7c7a2c40e952d
daniel12142007 <danijelahatzanov@gmail.com>
2024-05-20 18:06:23 +0600

    OOP

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 void main() {
   // String firstName = 'Daniel';
   // int age = 17;
@@ -33,12 +35,95 @@ void main() {
   // print(array);
   // var set = {1,1,2,3,4,5,6};//Set<int> сохраняет только уникальные значение
 
-  var array = [1, 2, 3, 4, 5, 6];
-  for (var element in array) {
-    print(element);
-  } //foreach in java
+  // var array = [1, 2, 3, 4, 5, 6];
+  // for (var element in array) {
+  //   print(element);
+  // } //foreach in java
 
-  array.forEach((el) {
-    print(el);
-  });
+  // array.forEach((el) {
+  //   print(el);
+  // });
+
+  var person = Person('Daniel', 17);
+  person.description();
+  var student = Student('Student', 16, '1', 'yes');
+  student.description();
+
+  University university = University('Tech University');
+
+  Course course1 = Course('Dart Programming');
+  Course course2 = Course('Flutter Development');
+
+  Student student1 = Student('Alice', 20, 'S12345', 'Math');
+  Student student2 = Student('Bob', 22, 'S67890', 'Physics');
+  Student student3 = Student('Charlie', 23, 'S54321', 'Chemistry');
+
+  course1.addStudent(student1);
+  course1.addStudent(student2);
+
+  course2.addStudent(student3);
+
+  university.addCourse(course1);
+  university.addCourse(course2);
+
+  university.listCourses();
+  university.listAllStudents();
+}
+
+class Person {
+  String name;
+  int age;
+  Person(this.name, this.age);
+  void description() {
+    print('Name: $name \nAge: $age');
+  }
+}
+
+class Student extends Person {
+  String studentId;
+  String major;
+  Student(super.name, super.age, this.studentId, this.major);
+  @override
+  void description() {
+    print('Name: $name \nAge: $age \nStudent ID: $studentId \nMajor: $major');
+  }
+}
+
+class Course {
+  String courseName;
+  List<Student> students;
+  Course(this.courseName) : students = [];
+  Student addStudent(Student student) {
+    students.add(student);
+    return student;
+  }
+
+  void listStudents() {
+    for (var element in students) {
+      print(element);
+    }
+  }
+}
+
+class University {
+  String universityName;
+  List<Course> courses;
+  University(this.universityName) : courses = [];
+  void addCourse(Course course) {
+    courses.add(course);
+  }
+
+  void listCourses() {
+    for (var element in courses) {
+      print(element);
+    }
+  }
+
+  void listAllStudents() {
+    for (var element in courses) {
+      for (var el in element.students) {
+        print(el);
+      }
+    }
+  }
 }
